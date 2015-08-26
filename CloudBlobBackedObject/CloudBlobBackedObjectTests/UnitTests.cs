d3d193837d55c9cd05627f91ad9a12feadd01641
David Nicholson <me@djnicholson.com>
2015-08-25 21:02:11 -0700

    rename unit test class

--- CloudBlobBackedObject/CloudBlobBackedObjectTests/UnitTests.cs
+++ CloudBlobBackedObject/CloudBlobBackedObjectTests/UnitTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CloudBlobBackedObjectTests
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTests
     {
         [TestMethod]
         public void TestMethod1()
