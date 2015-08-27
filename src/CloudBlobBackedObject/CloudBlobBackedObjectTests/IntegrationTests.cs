46c2a7ebbdd74a4e7fecfb567467c0030b12f631
David Nicholson <me@djnicholson.com>
2015-08-26 19:21:45 -0700

    integration tests placeholder

---
+++ src/CloudBlobBackedObject/CloudBlobBackedObjectTests/IntegrationTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CloudBlobBackedObjectTests
+{
+    [TestClass]
+    public class IntegrationTests
+    {
+        [TestMethod]
+        public void HelloWorld()
+        {
+            Assert.Inconclusive("No tests yet!");
+        }
+    }
+}
