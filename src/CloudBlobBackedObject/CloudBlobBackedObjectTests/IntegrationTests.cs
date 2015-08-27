5d08d0be9bb2cb06a02dc96827a378f9366bed52
David Nicholson <me@djnicholson.com>
2015-08-26 21:33:57 -0700

    more progress on tests

--- src/CloudBlobBackedObject/CloudBlobBackedObjectTests/IntegrationTests.cs
+++ src/CloudBlobBackedObject/CloudBlobBackedObjectTests/IntegrationTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Win32;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace CloudBlobBackedObjectTests
 {
@@ -9,25 +10,36 @@ namespace CloudBlobBackedObjectTests
         [TestInitialize]
         public void TestSetup()
         {
-            string storageKey = Registry.GetValue(
+            string storageAccountAndKey = Registry.GetValue(
                 @"HKEY_CURRENT_USER\SOFTWARE\djnicholson\CloudBlobBackedObject",
                 "StorageKey",
                 null) as string;
 
             // If this fails, run the following command:
-            //   reg add HKEY_CURRENT_USER\SOFTWARE\djnicholson\CloudBlobBackedObject /v StorageKey /t REG_SZ /f /d foo
+            //   reg add HKEY_CURRENT_USER\SOFTWARE\djnicholson\CloudBlobBackedObject /v StorageKey /t REG_SZ /f /d "<storageAccountName> <storage key>"
             // (where foo is an Azure Storage access key)
-            if (storageKey == null)
+            if (storageAccountAndKey == null)
             {
                 Assert.Fail(
                     @"HKEY_CURRENT_USER\SOFTWARE\djnicholson\CloudBlobBackedObject\StorageKey not found");
             }
+
+            
+            
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+
         }
 
         [TestMethod]
-        public void HelloWorld()
+        public void NewObject()
         {
             Assert.Inconclusive("No tests yet!");
+
+            ICloudBlob blob;
         }
     }
 }
