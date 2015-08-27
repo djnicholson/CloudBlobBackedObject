5bf2e1484650c1eb4128a4ef16c72a9cb3b2e4b9
David Nicholson <me@djnicholson.com>
2015-08-26 20:42:49 -0700

    read storage key from registry

--- src/CloudBlobBackedObject/CloudBlobBackedObjectTests/IntegrationTests.cs
+++ src/CloudBlobBackedObject/CloudBlobBackedObjectTests/IntegrationTests.cs
@@ -1,10 +1,29 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
 
 namespace CloudBlobBackedObjectTests
 {
     [TestClass]
     public class IntegrationTests
     {
+        [TestInitialize]
+        public void TestSetup()
+        {
+            string storageKey = Registry.GetValue(
+                @"HKEY_LOCAL_MACHINE\SOFTWARE\djnicholson\CloudBlobBackedObject",
+                "StorageKey",
+                null) as string;
+
+            // If this fails, run the following command:
+            //   reg add HKEY_LOCAL_MACHINE\SOFTWARE\djnicholson\CloudBlobBackedObject /v StorageKey /t REG_SZ /f /d foo
+            // (where foo is a Azure Storage access key)
+            if (storageKey == null)
+            {
+                throw new InternalTestFailureException(
+                    @"HKEY_LOCAL_MACHINE\SOFTWARE\djnicholson\CloudBlobBackedObject\StorageKey not found");
+            }
+        }
+
         [TestMethod]
         public void HelloWorld()
         {
