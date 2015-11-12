cb24edc5fc0e66d7eae94df58f69bafef263824b
David Nicholson <me@djnicholson.com>
2015-11-11 18:03:27 -0800

    pull MarshalledExceptionsThread into its own class

---
+++ src/CloudBlobBackedObject/CloudBlobBackedObject/MarshalledExceptionsThread.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace CloudBlobBackedObject
+{
+    internal class MarshalledExceptionsThread
+    {
+        public MarshalledExceptionsThread(Action code)
+        {
+            this.exception = null;
+            this.thread = new Thread(() =>
+            {
+                ThrowBackgroundException();
+
+                try
+                {
+                    code();
+                }
+                catch (Exception e)
+                {
+                    lock (this)
+                    {
+                        this.exception = e;
+                    }
+                }
+            });
+        }
+
+        public void Start()
+        {
+            this.thread.Start();
+        }
+
+        public void Join()
+        {
+            this.thread.Join();
+
+            ThrowBackgroundException();
+        }
+
+        ~MarshalledExceptionsThread()
+        {
+            ThrowBackgroundException();
+        }
+
+        private void ThrowBackgroundException()
+        {
+            if (this.exception == null)
+            {
+                return;
+            }
+
+            lock (this)
+            {
+                if (this.exception == null)
+                {
+                    return;
+                }
+
+                this.exception = null;
+
+                throw new AggregateException(
+                    "An exception was thrown by a background thread",
+                    this.exception);
+            }
+        }
+
+        private Thread thread;
+        private Exception exception;
+    }
+}
